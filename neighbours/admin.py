2f8e271e38653691c3d3ddc935e8e9ea6d6b17ed
feven <feven.m.woldu@gmail.com>
2019-08-08 16:10:39 +0300

    registered the postcontent model

--- neighbours/admin.py
+++ neighbours/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Neighbourhood,NeighbourhoodUser,Bussiness
+from .models import Neighbourhood,NeighbourhoodUser,Bussiness,Postcontent
 # Register your models here.
-
-
 
 admin.site.register(Neighbourhood)
 admin.site.register(NeighbourhoodUser)
 admin.site.register(Bussiness)
+admin.site.register(Postcontent)
