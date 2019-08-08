916b95824d83595e099a41a114d869d9fc12eb72
feven <feven.m.woldu@gmail.com>
2019-08-08 10:37:28 +0300

    - Changed the contents of the database and modified the Time zone.

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'neighbourhood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neighbour',
+        'USER': 'feven',
+    'PASSWORD':'123',
     }
 }
 
@@ -108,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
