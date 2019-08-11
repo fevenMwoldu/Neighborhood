761338fba25f8c9e55e79cda3e5b406fe0b5792d
feven <feven.m.woldu@gmail.com>
2019-08-11 21:31:20 +0300

    - Recreated my models - Migrated my models

--- neighbours/models.py
+++ neighbours/models.py
@@ -1,15 +1,18 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 
 class Neighbourhood(models.Model):
-    neighbourhood_name = models.CharField(max_length = 30)
-    neighbourhood_location = models.CharField(max_length =30)
+    name = models.CharField(max_length = 30)
+    location = models.CharField(max_length =30)
     occupants_count = models.IntegerField()
+    admin = models.ForeignKey(User)
 
     def __str__(self):
-        return self.neighbourhood_location
+        return 'Neighbourhood (name: {self.name}, location: {self.location}, occupants_count: {self.occupants_count}, admin: {self.admin})'.format(self=self)
 
     def save_neighbourhood(self):
         self.save()
@@ -17,29 +20,43 @@ class Neighbourhood(models.Model):
     def delete_neighbourhood(self):
         self.delete()
 
-class NeighbourhoodUser(models.Model):
-    username = models.CharField(max_length = 30)
-    user_id = models.IntegerField
-    user_email = models.CharField(max_length = 30)
+class CustomUser(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=500)
+    profile_pic = models.ImageField(upload_to='photos/')
+    user = models.ForeignKey(User)
     neighbourhood = models.ForeignKey(Neighbourhood)
 
     def __str__(self):
-        return self.username
+        return 'CustomUser (first_name: {self.first_name}, last_name: {self.last_name}, bio: {self.bio}, profile_pic: {self.profile_pic}, user: {self.user}, neighbourhood: {self.neighbourhood} )'.format(self=self)
 
-    def save_neighbourhooduser(self):
+    def save_customuser(self):
         self.save()
 
-    def delete_neighbourhooduser(self):
+    def delete_customuser(self):
         self.delete()
 
+    def fullname(self):
+        return '{self.last_name}, {self.first_name}'.format(self=self)
+
+    @classmethod
+    def user_has_profile(cls, user_id):
+        profiles = CustomUser.objects.filter(user_id=user_id)
+        return len(profiles) > 0
+
 class Bussiness(models.Model):
-    bussiness_name = models.CharField(max_length = 30)
-    bussiness_email = models.CharField(max_length = 30)
+    name = models.CharField(max_length = 30)
+    email = models.CharField(max_length = 30)
+    telephone = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
+    contact = models.ForeignKey(CustomUser)
     neighbourhood = models.ForeignKey(Neighbourhood)
-    neighbourhood_user = models.ForeignKey(NeighbourhoodUser)
+   
 
     def __str__(self):
-        return self.bussiness_name
+        return 'Business (name: {self.name}, email: {self.email}, telephone: {self.telephone}, address: {self.address}, category: {self.category}, contact: {self.contact}, neighbourhood: {self.neighbourhood})'.format(self=self)
 
     def save_bussiness(self):
         self.save()
@@ -48,40 +65,29 @@ class Bussiness(models.Model):
         self.delete()
 
     @classmethod
-    def search_by_bussinessname(cls,search_term):
-        bussinesses = cls.objects.filter(bussiness_name__icontains=search_term)
+    def search_by_name(cls,search_term):
+        bussinesses = cls.objects.filter(name__icontains=search_term)
         return bussinesses
 
-class Postcontent(models.Model):
+    @classmethod
+    def search_by_category(cls, search_term):
+        bussinesses = cls.objects.filter(name__icontains=search_term)
+        return bussinesses
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
     content = models.CharField(max_length = 800)
-    neighbouhood_user = models.ForeignKey(NeighbourhoodUser)
-   
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(CustomUser)
+    neighbourhood = models.ForeignKey(Neighbourhood)
+    post_pic = models.ImageField(upload_to='photos/')
 
     def __str__(self):
-        return self.content
+        return 'Post (title: {self.title}, content: {self.content}, created_on: {self.created_on}, created_by: {self.created_by}, neighbourhood: {self.neighbourhood})'.format(self=self)
 
-    def save_postcontent(self):
+    def save_post(self):
         self.save()
 
-    def delete_postcontent(self):
+    def delete_post(self):
         self.delete()
-
-class Profile(models.Model):
-    User_bio = models.CharField(max_length =300)
-    neighbouhood = models.ForeignKey(Neighbourhood)
-    neighbouhood_user = models.ForeignKey(NeighbourhoodUser)
-   
-
-    def __str__(self):
-        return self.User_bio
-
-    def save_profile(self):
-        self.save()
-
-    def delete_profile(self):
-        self.delete()
-
-class NeighbourhoodLetterRecipients(models.Model):
-    name = models.CharField(max_length = 30)
-    email = models.EmailField()
 
