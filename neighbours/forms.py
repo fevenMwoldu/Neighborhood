e80b099d7e758b7957a9b23ce24cf1b98d7f73aa
feven <feven.m.woldu@gmail.com>
2019-08-11 23:49:10 +0300

    Included a doc string.

--- neighbours/forms.py
+++ neighbours/forms.py
@@ -1,16 +1,26 @@
 from django import forms
 from .models import Neighbourhood, CustomUser, Bussiness, Post
 
-
+'''
+   Form for the Custom user
+'''
 class CustomUserForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         exclude = ['user']
 
+'''
+   Form for the Neighbourhood
+'''
+
 class NeighbourhoodForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
 
+
+'''
+   Form for Posting of events or anythng else
+'''
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -18,7 +28,21 @@ class PostForm(forms.ModelForm):
         exclude = ['neighbourhood', 'created_by']
 
 
+'''
+   Form for Adding bussinesesses
+'''
+
 class BussinessForm(forms.ModelForm):
     class Meta:
         model = Bussiness
-        exclude = ['neighbourhood', 'created_by']
+        exclude = ['neighbourhood']
+
+'''
+   Form for changing Neighbourhood
+'''
+class ChangeNeighbourhoodForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        exclude = ['first_name', 'last_name', 'bio', 'profile_pic', 'user']
+
+ 
