1d1406817e153ccfab2a5b26ba62d298f50aba24
feven <feven.m.woldu@gmail.com>
2019-08-11 21:37:31 +0300

    Created an add_profile, add_post, and add_bussiness definitions

--- neighbours/views.py
+++ neighbours/views.py
@@ -1,29 +1,89 @@
 from django.shortcuts import render
 from django.http  import HttpResponse,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from . models import Neighbourhood,NeighbourhoodUser,Bussiness,Profile,Postcontent,NeighbourhoodLetterRecipients
+from . models import Neighbourhood, CustomUser, Bussiness, Post
 from .email import send_welcome_email
-from .forms import NeighbourhoodForm
+from .forms import NeighbourhoodForm,CustomUserForm,PostForm,BussinessForm
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
     current_user = request.user 
-    form = NeighbourhoodForm(request.POST)
+
+    custom_user = CustomUser.objects.filter(user_id=current_user.id).first()
+
+    if custom_user is None:
+        return HttpResponseRedirect('profile')
+
+    posts = Post.objects.filter(neighbourhood_id=custom_user.neighbourhood.id)
+
+    is_admin = True if current_user == custom_user.neighbourhood.admin else False
+    
+    return render(request, 'index.html', {"posts": posts, "neighbourhood": custom_user.neighbourhood.name, "is_admin": is_admin})
+
+@login_required(login_url='/accounts/login/')
+def add_profile(request):
+    current_user = request.user
+
     if request.method == 'POST':
-        # form = NeighbourhoodForm(request.POST)
+        form = CustomUserForm(request.POST, request.FILES)
         if form.is_valid():
-            name = form.cleaned_data['your_name']
-            email = form.cleaned_data['email']
+            custom_user = form.save(commit=False)
+            custom_user.user = current_user
+            custom_user.save()
 
-            recipient = NeighbourhoodLetterRecipients(name = name,email =email)
-            recipient.save()
-            send_welcome_email(name,email)
-            HttpResponseRedirect('index')
+            send_welcome_email(custom_user.fullname(), current_user.email)
 
-    contents = Postcontent.objects.all()
-    bussinesses = Bussiness.objects.all()
-    return render(request, 'index.html',{"contents" : contents,"bussinesses" : bussinesses,"letterForm":form})
+            return HttpResponseRedirect('/')
+
+    else:
+        form = CustomUserForm()
+
+    return render(request, 'add_profile.html', {"form": form})
+
+
+@login_required(login_url='/account/login/')
+def add_post(request):
+    print('add_post is called ....')
+    current_user = request.user
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            
+            custom_user = CustomUser.objects.filter(user_id=current_user.id).first()
+            post.created_by = custom_user
+            post.neighbourhood = custom_user.neighbourhood
+
+            post.save()
+            print('Saved post {}'.format(post))
+
+            return HttpResponseRedirect('/')
+
+    return render(request, 'add_post.html', {"form": form})
+
+
+@login_required(login_url='/accounts/login/')
+def add_bussiness(request):
+    current_user = request.user
+
+    if request.method == 'POST':
+        form = BussinessForm(request.POST, request.FILES)
+        if form.is_valid():
+            custom_user = form.save(commit=False)
+            custom_user.user = current_user
+            custom_user.save()
+
+            
+
+            return HttpResponseRedirect('/')
+
+    else:
+        form = BussinessForm()
+
+    return render(request, 'add_bussiness.html', {"form": form})
 
 
 def search_results(request):
