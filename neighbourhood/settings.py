54179e741f4db45a7cc2b1b9e5c2b8f3e86ba133
feven <feven.m.woldu@gmail.com>
2019-08-12 02:15:00 +0300

    included tinymce

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'neighbours',
     'bootstrap4',
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
