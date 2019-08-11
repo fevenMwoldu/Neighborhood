e1e136d82b2eede9a1a4de867902e05bd92e974e
feven <feven.m.woldu@gmail.com>
2019-08-11 21:34:20 +0300

    Created  Forms for the CustomUser,Bussiness and Post models

--- neighbours/forms.py
+++ neighbours/forms.py
@@ -1,5 +1,24 @@
 from django import forms
+from .models import Neighbourhood, CustomUser, Bussiness, Post
+
+
+class CustomUserForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        exclude = ['user']
 
 class NeighbourhoodForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['neighbourhood', 'created_by']
+
+
+class BussinessForm(forms.ModelForm):
+    class Meta:
+        model = Bussiness
+        exclude = ['neighbourhood', 'created_by']
