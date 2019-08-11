afb9113bed6bc115d9ac128349aa83b6e1b6d610
feven <feven.m.woldu@gmail.com>
2019-08-12 00:32:33 +0300

    Added a doc string

--- neighbours/views.py
+++ neighbours/views.py
@@ -3,9 +3,13 @@ from django.http  import HttpResponse,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from . models import Neighbourhood, CustomUser, Bussiness, Post
 from .email import send_welcome_email
-from .forms import NeighbourhoodForm,CustomUserForm,PostForm,BussinessForm
+from .forms import NeighbourhoodForm,CustomUserForm,PostForm,BussinessForm,ChangeNeighbourhoodForm
 
 # Create your views here.
+'''
+    Definition for the index, main page
+'''
+
 @login_required(login_url='/accounts/login/')
 def index(request):
     current_user = request.user 
@@ -20,6 +24,11 @@ def index(request):
     is_admin = True if current_user == custom_user.neighbourhood.admin else False
     
     return render(request, 'index.html', {"posts": posts, "neighbourhood": custom_user.neighbourhood.name, "is_admin": is_admin})
+
+
+'''
+    Definition for the adding a profile
+'''
 
 @login_required(login_url='/accounts/login/')
 def add_profile(request):
@@ -41,6 +50,10 @@ def add_profile(request):
 
     return render(request, 'add_profile.html', {"form": form})
 
+
+'''
+    Definition for adding a post
+'''
 
 @login_required(login_url='/account/login/')
 def add_post(request):
@@ -65,6 +78,10 @@ def add_post(request):
     return render(request, 'add_post.html', {"form": form})
 
 
+'''
+    Definition for adding bussiness
+'''
+
 @login_required(login_url='/accounts/login/')
 def add_bussiness(request):
     current_user = request.user
@@ -72,11 +89,9 @@ def add_bussiness(request):
     if request.method == 'POST':
         form = BussinessForm(request.POST, request.FILES)
         if form.is_valid():
-            custom_user = form.save(commit=False)
-            custom_user.user = current_user
-            custom_user.save()
-
-            
+            business = form.save(commit=False)            
+            business.neighbourhood = CustomUser.objects.filter(user_id=current_user.id).first().neighbourhood
+            business.save()
 
             return HttpResponseRedirect('/')
 
@@ -86,11 +101,38 @@ def add_bussiness(request):
     return render(request, 'add_bussiness.html', {"form": form})
 
 
+'''
+    Definition for changing a neighbourhood
+'''
+
+@login_required(login_url='/accounts/login/')
+def change_neighbourhood(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ChangeNeighbourhoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            formdata = form.save(commit=False)
+
+            custom_user = CustomUser.objects.filter(user_id=current_user.id).first()
+            custom_user.neighbourhood = formdata.neighbourhood
+            custom_user.save()
+
+            return HttpResponseRedirect('/')
+
+    else:
+        form = ChangeNeighbourhoodForm()
+
+    return render(request, 'move.html', {"form": form})
+
+
+'''
+    Definition for searching bussiness,hospital or police station
+'''
 def search_results(request):
 
     if 'search_term' in request.GET and request.GET["search_term"]:
         search_term = request.GET.get("search_term")
-        searched_bussinesses = Bussiness.search_by_bussinessname(search_term)
+        searched_bussinesses = Bussiness.search_by_name(search_term)
         message = f"{search_term}"
 
         return render(request, 'search.html',{"message":message,"bussinesses": searched_bussinesses})
